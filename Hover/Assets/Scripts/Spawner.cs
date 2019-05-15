7068b798850929d11c28bd806ca32436e942e931
Obstkuchen <bloomeffect.pos@gmail.com>
2019-05-15 11:24:49 +0200

    project
    f

---
+++ Hover/Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone;
+
+ 
+
+    private void Start()
+    {
+        spawnSomething();
+    }
+
+   
+
+    void spawnSomething()
+
+
+    {
+
+        
+
+        whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocations[0].transform) as GameObject;
+        whatToSpawnClone[1] = Instantiate(whatToSpawnPrefab[1], spawnLocations[1].transform) as GameObject;
+        whatToSpawnClone[1] = Instantiate(whatToSpawnPrefab[1], spawnLocations[2].transform) as GameObject;
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
