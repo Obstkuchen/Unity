ed0a03c1951d13b42481b9209eac8ec2de69b555
Obstkuchen <bloomeffect.pos@gmail.com>
2020-02-26 14:42:48 +0100

    tesdt
    f

---
+++ Hover/Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    public Vector3 Spawnpoint
+    {
+        get
+        {
+            return Random.insideUnitSphere * 10f;
+        }
+    }
+
+}
+
